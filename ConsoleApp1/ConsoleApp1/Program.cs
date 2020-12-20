42408d337b4dae9bb58581bacbf221cf1686dc6d
fajer malallah <frmalallah9@gmail.com>
2020-12-20 20:36:06 +0300

    واجب

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "mark";
+            int maxhp = 250;
+            double characterspeed = 3;
+            int characterAge = 22;
+            string charctersuperpower1 = "visable";
+            string charactersuperpower2 = "speed";
+
+            Console.WriteLine("charctaer name is{0} and his max health is{1} his speed is {2} his age is{3} super power1 is{4} and his superpower2 is{5}" )
+
+
+
+
+        }
+    }
+}
